709f82bab4df12cd9f18c705fe4fca9714ce097a
NanditaSahu03 <nanditasahu031@gmail.com>
2022-08-31 14:46:10 +0000

    Changed token

--- provider.tf
+++ provider.tf
@@ -12,4 +12,8 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-a"
   access_token = "ya29.c.b0AXv0zTOIj2uTNSYsWtwHCHZlRCrNB-PTfV74KgIr_XFpnlkroXXJt5MSmCoZumQZI-g0HuNA0chMKWfKs-HQ2Y2mTIih4fur30QuROmKur3von9f2PBAKn40D9UINsCfBahZrVTyMa2r9GmXny_dQN0A1Rt8-mayrWH0qywCsn99GiN3Z94ldS4qyNToUzhtgDnD6MRuoTEac5Jav228M7Atdto_i5PhdhIURHPYnKpJKvThhsqYFliqHCgZQiRKcTzDgoaHbN0dWPuCCdjQMwP4mgovsOjKwRAjLWtchrT0mrwvrRXbBClSUbejJ_QM6MsGzL8yj_jzyTxlQzNjvRHrnLxFJFnrg1d434b-Wn3wUhX5F3bCODN205yS3pKAsRGOva4ck9gt7dGnhz8MgyhW_Bz4F50zP864vrbcH_QvO_7aOpEuSe0eXWZqt7XA_NkNBvweSSEc7J7x_7YIs5jS8LrkJA"
+
 }
+
+
+
